073f425909b2ca9d2a6f4e4c470b5b3e2cd96009
Hiruka Karasinghe <hiruka.karasinghe@gmail.com>
2023-10-26 22:47:58 +0530

    dashboard (incomplete)

---
+++ application/views/common/sidebar.php
@@ -0,0 +1,48 @@
+<div class="col-2 sidebarcont">
+<aside>
+    <div class="togglee">
+        <div class="logoo">
+            <img src="<?php echo site_url(); ?>images/logo-transparent.png">
+            <h2>Medi<span class="dangerr">Tech.</span></h2>
+        </div>
+        <div class="closee" id="close-btn">
+            <span class="material-symbols-sharp">
+                close
+            </span>
+        </div>
+    </div>
+
+    <div class="sidebarr">
+        <a href="#" class="list-unstyled">
+            <span class="material-icons">
+                dashboard
+            </span>
+            <h3>Dashboard</h3>
+        </a>
+        <a href="#" class="list-unstyled">
+            <span class="material-icons">
+                person_outline
+            </span>
+            <h3>Users</h3>
+        </a>
+        <a href="#" class="list-unstyled">
+            <span class="material-icons">
+                receipt_long
+            </span>
+            <h3>History</h3>
+        </a>
+        <a href="#" class="list-unstyled">
+            <span class="material-icons">
+                settings
+            </span>
+            <h3>Settings</h3>
+        </a>
+        <a href="<?php echo base_url().'login/logout' ?>" class="list-unstyled">
+            <span class="material-icons">
+                logout
+            </span>
+            <h3>Logout</h3>
+        </a>
+    </div>
+</aside>
+</div>
