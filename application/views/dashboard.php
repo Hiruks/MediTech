a1a5a29e89c0e2febf57888ad9fdeb1135c7ee68
Hiruka Karasinghe <hiruka.karasinghe@gmail.com>
2023-10-26 15:59:27 +0530

    dashboard side bar

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,11 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link href="<?php echo site_url(); ?>css/custom/dashboard.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Symbols+Sharp" rel="stylesheet" />
+
+    <title>Dashboard ~ MediTech</title>
 </head>
+
 <body>
-    <h1>Dashboard</h1>
+    <div class="container">
+        <aside>
+            <div class="toggle">
+                <div class="logo">
+                    <img src="<?php echo site_url(); ?>images/logo-transparent.png">
+                    <h2>Medi<span class="danger">Tech.</span></h2>
+                </div>
+                <div class="close" id="close-btn">
+                    <span class="material-symbols-sharp">
+                        close
+                    </span>
+                </div>
+            </div>
+
+            <div class="sidebar">
+                <a href="#">
+                    <span class="material-icons">
+                        dashboard
+                    </span>
+                    <h3>Dashboard</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons" >
+                        person_outline
+                    </span>
+                    <h3>Users</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons">
+                        receipt_long
+                    </span>
+                    <h3>History</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons">
+                        settings
+                    </span>
+                    <h3>Settings</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons">
+                        logout
+                    </span>
+                    <h3>Logout</h3>
+                </a>
+            </div>
+        </aside>
+
+    </div>
 </body>
+
 </html>
